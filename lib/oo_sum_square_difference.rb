0b034a738f37c2c3c6c591a3b1bb38589e08c6e5
gncurran <gnc115@gmail.com>
2015-12-03 21:11:53 -0500

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,19 @@
-# Implement your object-oriented solution here!
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+class SumSquareDifference
+
+  attr_accessor :n
+
+  def initialize(n)
+    @n = n
+  end
+
+  def difference
+    a = 0
+    (1..n).each { |x| a += x**2 }
+    b = 0
+    (1..n).each {|x| b += x}
+    squared = b**2
+    return (squared - a)
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,10 @@
-# Implement your procedural solution here!
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+def sum_square_difference(n)
+  a = 0
+  (1..n).each { |x| a += x**2 }
+  b = 0
+  (1..n).each {|x| b += x}
+  squared = b**2
+  return (squared - a)
+end
